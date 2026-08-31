0c786b98b7105d062d62bcdbaee160240a9ae4c4
Bhushan Malviya <atharvamalviya02@gmail.com>
2026-08-31 20:40:24 +0530

    Time: 3 ms (18.05%), Space: 50 MB (93.75%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -40,18 +40,73 @@ public:
 
         // }
         // return len;
-        int count =0;
-        int maxcount =0;
-        for(int i =0;i<nums.size();i++){
-            if(nums[i] == 1){
-                count ++;
-            }else{
-                count =0;
-            }
+        // int count =0;
+        // int maxcount =0;
+        // for(int i =0;i<nums.size();i++){
+        //     if(nums[i] == 1){
+        //         count ++;
+        //     }else{
+        //         count =0;
+        //     }
 
-            maxcount = max(count,maxcount);
+        //     maxcount = max(count,maxcount);
+        // }
+
+        // return maxcount;
+
+
+
+
+
+
+
+
+
+
+
+        long long count =0;
+        long long res =0;
+        int i =0;
+
+        // for(int i =0;i<nums.size();i++){
+        //     while(nums[i]==0){
+        //         i++;
+        //     }
+        //     while(nums[i]==1 and i<nums.size()){
+        //         count++;
+        //         i++;
+        //     }
+        //                 res = max(res,count);
+
+        //     count=0;
+
+
+        // }
+        while(i<nums.size()){
+            while(i<nums.size() and nums[i] == 0){
+                i++;
+            }
+            while(i<nums.size() and nums[i] == 1 ){
+                count++;
+
+                i++;
+            }
+            res = max(res,count);
+            count =0;
         }
 
-        return maxcount;
+
+
+return res;
+
+
+
+
+
+
+
+
+
+
     }
 };
