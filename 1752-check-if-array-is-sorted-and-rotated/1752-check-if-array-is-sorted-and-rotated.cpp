0b29078644a55e9a43e27231e590c5ab99daeda3
Bhushan Malviya <atharvamalviya02@gmail.com>
2026-08-31 19:33:47 +0530

    Time: 0 ms (100%), Space: 11.3 MB (55.9%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -45,23 +45,72 @@ public:
 
 
 
-        int count =0;
-        for(int i =0;i<nums.size()-1;i++){
-            if(nums[i+1]<nums[i]){
-                count++;
-            }
+        // int count =0;
+        // for(int i =0;i<nums.size()-1;i++){
+        //     if(nums[i+1]<nums[i]){
+        //         count++;
+        //     }
             
 
+        // }
+        // if(nums[nums.size()-1] > nums[0]){
+        //     count++;
+
+        // }
+
+        // if(count > 1){
+        //     return false;
+        // }
+        // return true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        int count =0;
+        for(int i =0;i<nums.size()-1;i++){
+            if(nums[i]>nums[i+1]){
+                count++;
+            }
+
+            
         }
         if(nums[nums.size()-1] > nums[0]){
             count++;
-
         }
 
         if(count > 1){
             return false;
         }
+
         return true;
+        
 
 
     }
