5013bbd3d05b0a62055225dbdc3c47eafc14dfbc
Bhushan Malviya <atharvamalviya02@gmail.com>
2026-08-31 20:16:53 +0530

    Time: 1 ms (31.63%), Space: 263.5 MB (10.71%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -12,18 +12,38 @@ public:
         // for(int i =0;i<nums.size();i++){
         //     nums[i] = narr[i];
         // }
-        int newk = k%nums.size();
 
-         int idx = nums.size()-newk;
-        vector<int>narr(nums.begin()+idx,nums.end());
 
-        for(int i =0;i<idx;i++){
-            narr.push_back(nums[i]);
-        }
 
-        for(int i =0;i<nums.size();i++){
-            nums[i] = narr[i];
-        }
+
+
+
+
+        // the k is number of time it is to be rotated and it can be greater than n 
+        // also k or 2k or 3k is same as arr will be same so 
+        // rotatiing k%n when k>n is  same 
+
+
+        // int newk = k%nums.size();
+
+        //  int idx = nums.size()-newk;
+        // vector<int>narr(nums.begin()+idx,nums.end());
+
+        // for(int i =0;i<idx;i++){
+        //     narr.push_back(nums[i]);
+        // }
+
+        // for(int i =0;i<nums.size();i++){
+        //     nums[i] = narr[i];
+        // }
+
+        k = k % nums.size();
+
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+
+
 
     }
 };
