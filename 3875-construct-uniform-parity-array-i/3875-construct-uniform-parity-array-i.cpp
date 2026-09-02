586d05841cf774e52e50bc81b3370394e36e4f09
Bhushan Malviya <atharvamalviya02@gmail.com>
2026-09-02 12:07:29 +0530

    Time: 0 ms (100%), Space: 30.3 MB (36.43%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        vector<int>nums2;
+
+        // for(int i =0;i<nums1.size();i++){
+        //     if(nums1[i] % 2 ==0){
+        //         nums2.push_back(nums1[i]);
+        //     }else{
+
+        //     }
+        // }
+    
+
+    return true;
+    }
+};
