596afe0098ea031f576084791a9b3d5e829ff851
Bhushan Malviya <atharvamalviya02@gmail.com>
2026-09-03 20:39:39 +0530

    Time: 0 ms (100%), Space: 11.6 MB (79.25%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int i =0;
-        int m =0;
-        int j = nums.size()-1;
-        while( m <= j){
-            if(nums[m] == 0){
-                swap(nums[i],nums[m]);
-                i++;
-                m++;
-                //
-            }
-            else if(nums[m] == 1){
-                m++;
-            }
-            else if(nums[m] == 2){
-                swap(nums[j],nums[m]);
-                j--;
-            }
-            // m++;
-            
-        }
-
+        // int i = 0;
+        // int m = 0;
+        // int j = nums.size() - 1;
+        // while (m <= j) {
+        //     if (nums[m] == 0) {
+        //         swap(nums[i], nums[m]);
+        //         i++;
+        //         m++;
+        //         //
+        //     } else if (nums[m] == 1) {
+        //         m++;
+        //     } else if (nums[m] == 2) {
+        //         swap(nums[j], nums[m]);
+        //         j--;
+        //     }
+        //     // m++;
+        // }
 
         // int i =0;
         // int j = nums.size()-1;
@@ -48,6 +44,54 @@ public:
         //     j--;
 
         // }
-        
+
+
+
+
+
+
+
+
+
+int start =0;
+int end = nums.size()-1;
+int check =0;
+
+while(check <= end){
+    if(nums[check] == 0 ){
+        swap(nums[check],nums[start]);
+        start++;
+        check++;
+
+    }else if(nums[check] == 1){
+        check++;
+        // start++;
+    }else{
+        swap(nums[check],nums[end]);
+        end--;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 };
