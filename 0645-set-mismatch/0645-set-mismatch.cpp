1751c4d95381831aeeef64e6f2dadb67771fb621
Bhushan Malviya <atharvamalviya02@gmail.com>
2026-09-12 11:25:08 +0530

    Time: 27 ms (26.87%), Space: 34.8 MB (11.57%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        // sort(nums.begin(),nums.end());
+        // int size = nums.size();
+        // int i =0;
+        // int num = 1;
+        // while(i < size){
+        //     if(num == nums[i]){
+        //         i++;
+        //         num++;
+        //     }else{
+        //         return {nums[i],num};
+        //     }
+        // }
+        // return {0,0};
+
+        int miss =0;
+        int id=0;
+
+
+        unordered_map<int,int>mpp;
+
+        for(auto x : nums){
+            mpp[x]++;
+        }
+
+
+    int temp = 1;
+            for(int i=0;i<nums.size();i++){
+                if(mpp.contains(temp)){
+                    if(mpp[temp] == 2){
+                    id = temp;
+
+                }
+                }else{
+                    miss = temp;
+
+                }
+
+                    temp++;
+                
+
+            }
+
+return {id,miss};
+
+
+
+
+    }
+};
