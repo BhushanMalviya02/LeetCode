d5924bfa2ec881b4d9cff0bcda19e273212ff41f
Bhushan Malviya <atharvamalviya02@gmail.com>
2026-09-20 22:45:35 +0530

    Time: 0 ms (100%), Space: 15.1 MB (96.71%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,66 +1,103 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int start =0;
-        int end = nums.size()-1;
-        int n = nums.size();
+        // int start =0;
+        // int end = nums.size()-1;
+        // int n = nums.size();
 
-        while(start<= end){
-            int mid = (start+end)/2;
+        // while(start<= end){
+        //     int mid = (start+end)/2;
 
             
-            if(target == nums[n-1]){
-                return n-1;
+        //     if(target == nums[n-1]){
+        //         return n-1;
 
 
-            }else if(target < nums[n-1]){
+        //     }else if(target < nums[n-1]){
                 
 
-                if(nums[mid] > nums[n-1]){
-                    start=mid+1;
+        //         if(nums[mid] > nums[n-1]){
+        //             start=mid+1;
 
+        //         }else{
+        //             if(nums[mid] == target){
+        //                 return mid;
+        //             }else{
+        //                 if(nums[mid] > target){
+        //                 end = mid-1;
+
+        //                 }else{
+        //                     start = mid+1;
+        //                 }
+        //             }
+
+
+        //         }
+
+
+        //     }else{
+
+        //         if(nums[mid] < nums[n-1]){
+        //             end = mid-1;
+        //         }else{
+        //             if(nums[mid] == target){
+        //                 return mid;
+        //             }else{
+        //                 if(nums[mid] < target){
+        //                     start = mid+1;
+        //                 }else{
+        //                     end = mid-1;
+        //                 }
+        //             }
+        //         }
+
+
+
+        //     }
+
+        // }
+
+        // return -1;
+
+
+
+        int start =0;
+        int end = nums.size()-1;
+        int n =nums.size();
+
+        while(start <= end){
+            int mid = (start+end)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            
+            if(nums[mid] > nums[n-1]){
+                if(target > nums[mid]){
+                    start = mid+1;
                 }else{
-                    if(nums[mid] == target){
-                        return mid;
+                    if(target >= nums[0]){
+                        end =mid-1;
                     }else{
-                        if(nums[mid] > target){
-                        end = mid-1;
-
-                        }else{
-                            start = mid+1;
-                        }
+                        start =mid+1;
                     }
-
-
                 }
-
-
-            }else{
-
-                if(nums[mid] < nums[n-1]){
+            }
+            else{
+                if(target < nums[mid]){
                     end = mid-1;
                 }else{
-                    if(nums[mid] == target){
-                        return mid;
+                    if(target <= nums[n-1]){
+                                                start =mid+1;
+
                     }else{
-                        if(nums[mid] < target){
-                            start = mid+1;
-                        }else{
-                            end = mid-1;
-                        }
+                                                end = mid-1;
+
                     }
                 }
 
-
-
             }
-
         }
-
         return -1;
-
-
-
-      
     }
 };
