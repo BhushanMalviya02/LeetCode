3b6adeb7437c1d896ab222893ed64d3795ce2bf9
Bhushan Malviya <atharvamalviya02@gmail.com>
2026-09-23 13:25:29 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.48%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // int start=0;
-        // int end =nums.size()-1;
+        // // int start=0;
+        // // int end =nums.size()-1;
 
-        int reader =0;
+        // int reader =0;
+        // int writer =0;
+
+        // while(reader<nums.size()){
+        //     // while(nums[start] != 0){
+        //     //     start++;
+        //     // }
+        //     // while(nums[end] == 0){
+        //     //     end--;
+        //     // }
+        //     // swap(nums[start],nums[end]);
+        //     // start++;
+        //     // end--;
+
+        //     if(nums[reader] ==0){
+        //         reader++;
+        //     }else{
+        //         swap(nums[reader],nums[writer]);
+        //         writer++;
+        //         reader++;
+        //     }
+        // }
+
+
+
+
+
+
+
+
+
+
+        int reader=0;
         int writer =0;
 
-        while(reader<nums.size()){
-            // while(nums[start] != 0){
-            //     start++;
-            // }
-            // while(nums[end] == 0){
-            //     end--;
-            // }
-            // swap(nums[start],nums[end]);
-            // start++;
-            // end--;
 
-            if(nums[reader] ==0){
+        while(reader < nums.size()){
+            if(reader <nums.size() and nums[reader] == 0){
                 reader++;
             }else{
                 swap(nums[reader],nums[writer]);
@@ -26,5 +49,7 @@ public:
                 reader++;
             }
         }
+
+        // return writer;
     }
 };
